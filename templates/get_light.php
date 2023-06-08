4c3c47c1c8a1389aa78a7edcbbb0493d3298d7be
TuongVy00 <vothituongvy2906@gmail.com>
2023-06-08 13:57:20 +0700

    update complete

---
+++ templates/get_light.php
@@ -0,0 +1,11 @@
+<?php
+include("config/config.php");
+include("config/firebaseRDB.php");
+$db = new firebaseRDB($databaseURL);
+session_start();
+
+$data1 = $db->retrieve("system", "id_system", null, 1);
+$data1 = json_decode($data1, 1);
+
+echo $data1['light']; 
+?>
